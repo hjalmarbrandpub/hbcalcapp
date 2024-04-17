a674425313daf3f917a815f3b4c5c5538555e86d
hjalmarbrandpub <130356548+hjalmarbrandpub@users.noreply.github.com>
2024-04-17 11:47:27 +0200

    Update hbcalcapp.py

--- hb-package/hb-calc-app/hbcalcapp.py
+++ hb-package/hb-calc-app/hbcalcapp.py
@@ -1,1 +1,12 @@
+"""
+Calculator library containing basic math operations.
+"""
 
+
+def add(first_term, second_term):
+    """Return the sum of the inputs."""
+    return first_term + second_term
+
+def subtract(first_term, second_term):
+    """Return the difference of the inputs."""
+    return first_term - second_term
